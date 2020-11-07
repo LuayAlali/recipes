0318f659db4879b4dcba02b7cd26e7f3ae2dec89
Luay Alali <luayalali@gmail.com>
2020-11-07 12:15:24 -0800

    created an app component, added a basic form.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+
+  const APP_ID = 'db707132';
+  const APP_KEY = '6568bb4a4fed8cd474fb88ddd6b025fe';
+  
+  const exampleReq = 
+   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input  className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <h1>Hello React</h1>
     </div>
+
   );
+
 }
 
 export default App;
