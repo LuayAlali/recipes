cb3053f27abd277a6bcb8415c3e0b1952d25a543
Luay Alali <luayalali@gmail.com>
2020-11-07 12:28:28 -0800

    request is sent just once. on page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -6,18 +6,27 @@ const App = () => {
 
   const APP_ID = 'db707132';
   const APP_KEY = '6568bb4a4fed8cd474fb88ddd6b025fe';
-  
   const exampleReq = 
    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('effect has been run');
+  },[]);
+
+
+
   return(
     <div className="App">
       <form className="search-form">
         <input  className="search-bar" type="text"/>
-        <button className="search-button" type="submit">Search</button>
+        <button 
+         className="search-button" 
+         type="submit">Search</button>
       </form>
-      <h1>Hello React</h1>
+     
+      <h1 onClick = {() => setCounter(counter +1)}>{counter}</h1>
     </div>
 
   );
