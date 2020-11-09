280da5c2dbc2f0b6e6c65f8d7ef79641eb1c0a8f
Luay Alali <luayalali@gmail.com>
2020-11-09 12:52:40 -0800

    added ingredients to recipe.  added some basic styling

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,10 +3,15 @@ import React from 'react';
 
 
 
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return(
         <div>
             <h1>{title}</h1>
+            <ul>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ul>
             <p>{calories}</p>
             <img src={image} alt=""/>
         </div>
