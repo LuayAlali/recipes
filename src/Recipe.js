cbe98e6bb06e791ab6b7e4264a585ad9fb20eda8
Luay Alali <luayalali@gmail.com>
2020-11-09 14:06:39 -0800

    created a css style module for recipe and imported it to Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,19 +1,19 @@
 import React from 'react';
-
+import style from './recipe.module.css';
 
 
 
 const Recipe = ({title, calories, image, ingredients}) => {
     return(
-        <div>
-            <h1>{title}</h1>
+        <div className={style.recipe}>
+            <h1 >{title}</h1>
             <ul>
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>
                 ))}
             </ul>
             <p>{calories}</p>
-            <img src={image} alt=""/>
+            <img src={image} alt="" className={style.image}/>
         </div>
     );
 }
